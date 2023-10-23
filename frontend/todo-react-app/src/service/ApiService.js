a8398bd512c5c8bda6ce149ee7c1560bfa5d9b75
Gyujin Kim <kjkj268@naver.com>
2023-10-23 23:27:41 +0900

    로컬 스토리지를 이용한 엑세스 토큰 관리

--- frontend/todo-react-app/src/service/ApiService.js
+++ frontend/todo-react-app/src/service/ApiService.js
@@ -3,12 +3,20 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  // 로컬 스토리지에서 ACCESS TOKEN 가져오기
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken !== null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
-    method: method,
+    method: method
   };
   if (request) {
     // GET method
@@ -20,8 +28,7 @@ export function call(api, method, request) {
     }else if (response.status === 403) {
       window.location.href = "/login"; // redirect
     } else {
-      Promise.reject(response);
-      throw Error(response);
+      new Error(response);
     }
   }).catch((error) => {
     console.log("http error");
@@ -33,6 +40,8 @@ export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO)
     .then((response) => {
       if (response.token) {
+        // 로컬 스토리지에 토큰 저ㅇ
+        localStorage.setItem("ACCESS_TOKEN", response.token);
         // token이 존재하는 경우 Todo 화면으로 리다이렉트
         window.location.href = "/";
       }
